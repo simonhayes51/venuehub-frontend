05c16e894db266e7073f5726bfcba0a07ee47c42
simonhayes51 <simonhayes_@hotmail.com>
2025-10-28 00:17:24 +0000

    Fix ShortlistDrawer import to ../context/ShortlistContext.jsx

--- src/components/ShortlistDrawer.jsx
+++ src/components/ShortlistDrawer.jsx
@@ -1,5 +1,5 @@
-﻿import { useShortlist } from ".../context/ShortlistContext.jsx";
-import { useCompare } from ".../context/CompareContext.jsx";
+﻿import { useShortlist } from "../context/ShortlistContext.jsx";
+import { useCompare } from "../context/CompareContext.jsx";
 import { Link } from "react-router-dom";
 import { FaXmark } from "react-icons/fa6";
 
