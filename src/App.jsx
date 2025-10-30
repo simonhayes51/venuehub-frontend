6a611759bde4c13d9526e4c1dc46e03a99929ba8
simonhayes51 <simonhayes_@hotmail.com>
2025-10-30 01:44:06 +0000

    fix(routing): correct Admin import path and routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
-﻿import React, { useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import ShortlistBubble from './components/ShortlistBubble';
-import Home from './pages/Home';
-import Acts from './pages/Acts';
-import ActDetails from './pages/ActDetails';
-import Venues from './pages/Venues';
-import Pricing from './pages/Pricing';
-import Search from './pages/Search';
-import Admin from './pages\Admin';
-import { initStore } from './data/store';
+﻿import React, { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 
-export default function App(){
-  useEffect(()=>{ initStore(); },[]);
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import ShortlistBubble from "./components/ShortlistBubble";
+
+import Home from "./pages/Home";
+import Acts from "./pages/Acts";
+import ActDetails from "./pages/ActDetails";
+import Venues from "./pages/Venues";
+import Pricing from "./pages/Pricing";
+import Search from "./pages/Search";
+import Admin from "./pages/Admin";
+
+import { initStore } from "./data/store";
+
+export default function App() {
+  useEffect(() => { initStore(); }, []);
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/acts' element={<Acts/>}/>
-        <Route path='/act/:id' element={<ActDetails/>}/>
-        <Route path='/venues' element={<Venues/>}/>
-        <Route path='/pricing' element={<Pricing/>}/>
-        <Route path='/search' element={<Search/>}/>
-        <Route path='/admin' element={<Admin/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/acts" element={<Acts />} />
+        <Route path="/act/:id" element={<ActDetails />} />
+        <Route path="/venues" element={<Venues />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
       <Footer />
       <ShortlistBubble />
