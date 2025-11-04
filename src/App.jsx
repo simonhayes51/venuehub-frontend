eb4f0ec3cd7f1cdf9003aecdb7a1967d66af610c
simonhayes51 <simonhayes_@hotmail.com>
2025-11-04 19:02:17 +0000

    Set up routing in App component with React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,26 @@
-﻿<<< FULL CODE YOU PASTED ABOVE GOES HERE (the big cassette-style React app) >>>
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "./index.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Acts from "./pages/Acts";
+import Venues from "./pages/Venues";
+import Pricing from "./pages/Pricing";
+import Search from "./pages/Search";
+import Admin from "./pages/Admin";
+
+export default function App(){
+  return (
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/acts" element={<Acts/>}/>
+        <Route path="/venues" element={<Venues/>}/>
+        <Route path="/pricing" element={<Pricing/>}/>
+        <Route path="/search" element={<Search/>}/>
+        <Route path="/admin" element={<Admin/>}/>
+        <Route path="*" element={<Navigate to="/" replace />}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
